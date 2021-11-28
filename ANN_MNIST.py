431536e80cfccdbf267261973860fb4d63515bc5
Chris B <chrisberry888@gmail.com>
2021-11-27 21:53:12 -0500

    Made it work with sklearn and a regular neural net; accuracy of about 97%.

--- ANN_MNIST.py
+++ ANN_MNIST.py
@@ -2,6 +2,10 @@ import idx2numpy
 import numpy as np
 import matplotlib.pyplot as plt
 import os, sys
+from sklearn.neural_network import MLPClassifier
+import random
+
+random.seed(11)
 
 plt.ion()
 path = os.path.abspath(os.path.dirname(sys.argv[0]))
@@ -16,7 +20,23 @@ train_labels = idx2numpy.convert_from_file(train_label_file)
 test_images = idx2numpy.convert_from_file(test_image_file)
 test_labels = idx2numpy.convert_from_file(test_label_file)
 
-plt.imshow(test_images[4], cmap=plt.cm.binary)
-plt.draw()
+train_images = [x.flatten() for x in train_images]
+test_images = [x.flatten() for x in test_images]
 
-print(test_labels[:10])
+#plt.imshow(train_images[4], cmap=plt.cm.binary)
+#plt.draw()
+print(len(train_images[0]))
+#print(train_labels[:10])
+#print(train_images[4])
+print(1)
+clf = MLPClassifier(hidden_layer_sizes=())
+clf1 = MLPClassifier(hidden_layer_sizes=(100,))
+print(2)
+clf.fit(train_images, train_labels)
+print(3)
+print(clf.score(test_images,test_labels))
+print(4)
+clf1.fit(train_images, train_labels)
+print(5)
+print(clf1.score(test_images,test_labels))
+print(6)
